84ec26e6c628671a8583373a57c1c7b38fa8b37c
Daniel Urra <danielurra75@gmail.com>
2024-03-14 05:05:02 -0500

    Add files via upload

---
+++ CustomButtonView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomButtonView.swift
+//  capitalizationapp
+//
+//  Created by Eusebio Taba on 3/11/24.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    
+    var titleDanny: String
+    var colorDanny: Color
+    
+    var body: some View {
+        Text(titleDanny)
+            .font(.body)
+            .bold()
+            .padding()
+            .background(colorDanny)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+    }
+}
+
+#Preview {
+    CustomButtonView(titleDanny: "Tap Me", colorDanny: .blue)
+}
+
